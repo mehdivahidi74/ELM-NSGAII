aa1d0fd735b4ae9a27b473ff9fcd09a3a72ad284
Mehdi <109763384+mehdivahidi74@users.noreply.github.com>
2024-09-28 01:20:01 +0330

    Update ELM-NSGAII.py

--- ELM-NSGAII.py
+++ ELM-NSGAII.py
@@ -19,7 +19,7 @@ st.image(Breakwater)
 # RealCr = pd.read_excel('Weight-Cr-Real.xlsx') """
 P = 5
 M = 20
-Model = st.selectbox('Select Output' , ['Transmission Coefficient' , 'Reflection Coefficient' , 'Maximum Pressure Force Perforated Front Wall' , 'Maximum Pressure Force Perforated Back Wall'])
+Model = st.selectbox('Select Output' , ['Transmission Coefficient' , 'Reflection Coefficient'])
 if Model == 'Transmission Coefficient':
     def user_inputs_EX():
         st.subheader('Select Variable (cm)')
@@ -158,88 +158,88 @@ if Model == 'Reflection Coefficient':
 #     PredCr = np.matmul(H , BetaRealCr)
 #     st.subheader('Reflection Coefficient Prediction (Cr)')
 #     st.write(PredCr) """
-if Model == 'Maximum Pressure Force Perforated Front Wall':
-    def user_inputs_EX():
-        st.subheader('Select Variable (cm)')
-        B = st.sidebar.slider('Chamber Width (B)',14 , 224 , 40)
-        IH = st.sidebar.slider('Impermeable Height (IH)',4.0 , 20.0 , 12.0 , step = 0.1)
-        H = st.sidebar.slider('Incident Wave Height (H)',4.00 ,15.00 , 8.00 , step = 0.01)
-        L = st.sidebar.slider('Wavelength (L)',56.0 , 454.0 , 283.59 , step = 0.1)
-        h = st.sidebar.slider('Water depth (h)',1.00 , 100.00 , 40.00 , step = 0.01)
+#if Model == 'Maximum Pressure Force Perforated Front Wall':
+ #   def user_inputs_EX():
+  #      st.subheader('Select Variable (cm)')
+ #       B = st.sidebar.slider('Chamber Width (B)',14 , 224 , 40)
+  #      IH = st.sidebar.slider('Impermeable Height (IH)',4.0 , 20.0 , 12.0 , step = 0.1)
+ #          H = st.sidebar.slider('Incident Wave Height (H)',4.00 ,15.00 , 8.00 , step = 0.01)
+     #   L = st.sidebar.slider('Wavelength (L)',56.0 , 454.0 , 283.59 , step = 0.1)
+      #  h = st.sidebar.slider('Water depth (h)',1.00 , 100.00 , 40.00 , step = 0.01)
         # def func(y , T):
         #     eq = [(981/(2*np.pi)) * T**2 * np.tanh((2*np.pi/(y[0])) * 40 )- y[0]]
         #     return eq
         # L = st.sidebar.slider('Wavelength (L)', (fsolve(func , [1] , T).tolist())[0])
-        return B , IH , H , L , h
-    B , IH , H , L , h = user_inputs_EX()
-    MaxEX = [('B / h' , 5.605) , ('IH / h' , 0.400000) , ('H / L' , 0.071184) , ('H / h' , 0.300000)]
-    MinEX = [('B / h' , 0.35) , ('IH / h' , 0.100000) , ('H / L' , 0.022066) , ('H / h' , 0.100000) ]
-    st.subheader('Non-dimensional Inputs Variables')
-    st.write('B / h: ' ,"{:.5f}".format(B/h) )
-    st.write('IH / h: ' ,"{:.5f}".format(IH/h) )
-    st.write('H / L: ' ,"{:.5f}".format(H/L) )
-    st.write('H / h: ' ,"{:.5f}".format(H/h) )
-    X = np.array([[B/h,IH/h,H/L,H/h]])
-    for i in range (np.shape(X)[0]):
-        for j in range (np.shape(X)[1]):
-            X[i][j] = (X[i][j] - MinEX[j][1])/ (MaxEX[j][1] - MinEX[j][1])
-    #Evaluate Ct in Experimental Scale
-    one = np.ones((np.shape(X)[0] , 1))*-1
-    X = np.append(one,X , axis = 1)
-    WEXF1 = EXF1.iloc[0:20 , 0:5]
-    WEXF1 = np.transpose(WEXF1)
-    WEXF1 = np.array(WEXF1)
-    BetaEXF1 = EXF1.iloc[0:21 , 5:6]
-    BetaEXF1 = np.array(BetaEXF1)
-    H = np.matmul(X , WEXF1)
-    H = np.tanh(H)
-    one = np.ones((np.shape(X)[0] , 1))*-1
-    H = np.append(one,H , axis = 1)
-    PredF1 = np.matmul(H , BetaEXF1)
-    st.subheader('Maximum Pressure Force Perforated Front Wall (F*1)')
-    st.subheader("{:.5f}".format(PredF1.tolist()[0][0]))
+      #  return B , IH , H , L , h
+    #B , IH , H , L , h = user_inputs_EX()
+    #MaxEX = [('B / h' , 5.605) , ('IH / h' , 0.400000) , ('H / L' , 0.071184) , ('H / h' , 0.300000)]
+   # MinEX = [('B / h' , 0.35) , ('IH / h' , 0.100000) , ('H / L' , 0.022066) , ('H / h' , 0.100000) ]
+    #st.subheader('Non-dimensional Inputs Variables')
+ #   st.write('B / h: ' ,"{:.5f}".format(B/h) )
+  #  st.write('IH / h: ' ,"{:.5f}".format(IH/h) )
+   # st.write('H / L: ' ,"{:.5f}".format(H/L) )
+    #st.write('H / h: ' ,"{:.5f}".format(H/h) )
+#    X = np.array([[B/h,IH/h,H/L,H/h]])
+ #   for i in range (np.shape(X)[0]):
+ #       for j in range (np.shape(X)[1]):
+  #          X[i][j] = (X[i][j] - MinEX[j][1])/ (MaxEX[j][1] - MinEX[j][1])
+  #  #Evaluate Ct in Experimental Scale
+   # one = np.ones((np.shape(X)[0] , 1))*-1
+    #X = np.append(one,X , axis = 1)
+    #WEXF1 = EXF1.iloc[0:20 , 0:5]
+    #WEXF1 = np.transpose(WEXF1)
+    #WEXF1 = np.array(WEXF1)
+    #BetaEXF1 = EXF1.iloc[0:21 , 5:6]
+    #BetaEXF1 = np.array(BetaEXF1)
+  #  H = np.matmul(X , WEXF1)
+  #  H = np.tanh(H)
+   # one = np.ones((np.shape(X)[0] , 1))*-1
+   # H = np.append(one,H , axis = 1)
+  #  PredF1 = np.matmul(H , BetaEXF1)
+  #  st.subheader('Maximum Pressure Force Perforated Front Wall (F*1)')
+  #  st.subheader("{:.5f}".format(PredF1.tolist()[0][0]))
     
     
-if Model == 'Maximum Pressure Force Perforated Back Wall':
-    def user_inputs_EX():
-        st.subheader('Select Variable (cm)')
-        B = st.sidebar.slider('Chamber Width (B)',14 , 224 , 40)
-        IH = st.sidebar.slider('Impermeable Height (IH)',4.0 , 20.0 , 12.0 , step = 0.1)
-        H = st.sidebar.slider('Incident Wave Height (H)',4.00 ,15.00 , 8.00 , step = 0.01)
-        L = st.sidebar.slider('Wavelength (L)',56.0 , 454.0 , 283.59 , step = 0.1)
-        h = st.sidebar.slider('Water depth (h)',1.00 , 100.00 , 40.00 , step = 0.01)
+#if Model == 'Maximum Pressure Force Perforated Back Wall':
+ #   def user_inputs_EX():
+   #     st.subheader('Select Variable (cm)')
+   #     B = st.sidebar.slider('Chamber Width (B)',14 , 224 , 40)
+   #     IH = st.sidebar.slider('Impermeable Height (IH)',4.0 , 20.0 , 12.0 , step = 0.1)
+   #     H = st.sidebar.slider('Incident Wave Height (H)',4.00 ,15.00 , 8.00 , step = 0.01)
+   #     L = st.sidebar.slider('Wavelength (L)',56.0 , 454.0 , 283.59 , step = 0.1)
+   #     h = st.sidebar.slider('Water depth (h)',1.00 , 100.00 , 40.00 , step = 0.01)
         # def func(y , T):
         #     eq = [(981/(2*np.pi)) * T**2 * np.tanh((2*np.pi/(y[0])) * 40 )- y[0]]
         #     return eq
         # L = st.sidebar.slider('Wavelength (L)', (fsolve(func , [1] , T).tolist())[0])
-        return B , IH , H , L , h
-    B , IH , H , L , h = user_inputs_EX()
-    MaxEX = [('B / h' , 5.605) , ('IH / h' , 0.400000) , ('H / L' , 0.071184) , ('H / h' , 0.300000)]
-    MinEX = [('B / h' , 0.35) , ('IH / h' , 0.100000) , ('H / L' , 0.022066) , ('H / h' , 0.100000) ]
-    st.subheader('Non-dimensional Inputs Variables')
-    st.write('B / h: ' ,"{:.5f}".format(B/h) )
-    st.write('IH / h: ' ,"{:.5f}".format(IH/h) )
-    st.write('H / L: ' ,"{:.5f}".format(H/L) )
-    st.write('H / h: ' ,"{:.5f}".format(H/h) )
-    X = np.array([[B/h,IH/h,H/L,H/h]])
-    for i in range (np.shape(X)[0]):
-        for j in range (np.shape(X)[1]):
-            X[i][j] = (X[i][j] - MinEX[j][1])/ (MaxEX[j][1] - MinEX[j][1])
+    #    return B , IH , H , L , h
+ #   B , IH , H , L , h = user_inputs_EX()
+ #   MaxEX = [('B / h' , 5.605) , ('IH / h' , 0.400000) , ('H / L' , 0.071184) , ('H / h' , 0.300000)]
+ #   MinEX = [('B / h' , 0.35) , ('IH / h' , 0.100000) , ('H / L' , 0.022066) , ('H / h' , 0.100000) ]
+ #   st.subheader('Non-dimensional Inputs Variables')
+ #   st.write('B / h: ' ,"{:.5f}".format(B/h) )
+ #   st.write('IH / h: ' ,"{:.5f}".format(IH/h) )
+ #   st.write('H / L: ' ,"{:.5f}".format(H/L) )
+  #  st.write('H / h: ' ,"{:.5f}".format(H/h) )
+ #   X = np.array([[B/h,IH/h,H/L,H/h]])
+ #   for i in range (np.shape(X)[0]):
+   #     for j in range (np.shape(X)[1]):
+ #           X[i][j] = (X[i][j] - MinEX[j][1])/ (MaxEX[j][1] - MinEX[j][1])
     #Evaluate Ct in Experimental Scale
-    one = np.ones((np.shape(X)[0] , 1))*-1
-    X = np.append(one,X , axis = 1)
-    WEXF2 = EXF2.iloc[0:20 , 0:5]
-    WEXF2 = np.transpose(WEXF2)
-    WEXF2 = np.array(WEXF2)
-    BetaEXF2 = EXF2.iloc[0:21 , 5:6]
-    BetaEXF2 = np.array(BetaEXF2)
-    H = np.matmul(X , WEXF2)
-    H = np.tanh(H)
-    one = np.ones((np.shape(X)[0] , 1))*-1
-    H = np.append(one,H , axis = 1)
-    PredF2 = np.matmul(H , BetaEXF2)
-    st.subheader('Maximum Pressure Force Perforated Back Wall (F*2)')
-    st.subheader("{:.5f}".format(PredF2.tolist()[0][0]))
+ #   one = np.ones((np.shape(X)[0] , 1))*-1
+ #   X = np.append(one,X , axis = 1)
+ #   WEXF2 = EXF2.iloc[0:20 , 0:5]
+ #   WEXF2 = np.transpose(WEXF2)
+ #   WEXF2 = np.array(WEXF2)
+ #   BetaEXF2 = EXF2.iloc[0:21 , 5:6]
+ #   BetaEXF2 = np.array(BetaEXF2)
+  #  H = np.matmul(X , WEXF2)
+ #   H = np.tanh(H)
+ #   one = np.ones((np.shape(X)[0] , 1))*-1
+  #  H = np.append(one,H , axis = 1)
+ #   PredF2 = np.matmul(H , BetaEXF2)
+ #   st.subheader('Maximum Pressure Force Perforated Back Wall (F*2)')
+ #   st.subheader("{:.5f}".format(PredF2.tolist()[0][0]))
     
     
     
